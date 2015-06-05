81f3eadb32ad90cc55451a1a43ba8af6d5aa9f98
Andy Baird <Andy.Baird@icloud.com>
2015-06-05 12:30:49 -0400

    Setup function to parse inputs for GitHub. GH Api working and outputting to DOM, unstyled

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,15 @@
 (function() {
+//function to display user input in output element
 
-//  $('.user-form').on('submit', function(e){
-//    e.preventDefault();
-//    var input = $('.user-input').val();
-//    $('.list-user').text(input); 
-//    $('.user-input').val(''); 
-//  });
 
-  $('.user-form').on('submit', function newLi(e){
-    e.preventDefault();
+//master function to listen and parse all inputs
+$('.user-form').on('submit', function parseInputs(e){
+  e.preventDefault();
+    var input = $('.user-input').val().split(': ');
+    console.log(input[0]);
+    	if (input[0] == "gh") {
+        getGitHub();
+      }else{
         var ul = document.querySelector('.output-list');
         var li = document.createElement("li");
         var input = $('.user-input').val();
@@ -16,7 +17,33 @@
           ul.appendChild(li);
           $('.list-user').last().text(input);
           $('.user-input').val(''); 
-  });
+    };
+});
+//function to query GitHub
+function getGitHub (){
+  var input = $('.user-input').val().split(': ');
+  var ghLogin = input[1];
+  console.log(ghLogin);
+    $.getJSON('https://api.github.com/users/' + ghLogin)
+      .done(showUser)
+      .fail(showError);
+  };
+  
+  function showUser(user) {
+    show('gh-user-template', user);
+  }
 
-
+  function show(template, model) {
+    var fn = _.template($('#' + template).html(), { variable: 'm' });
+    $('.output-list').html(fn(model));
+  } 
+  function showError(req, status, err) {
+    err = err || {};
+    err.message = err.message || status;
+    console.log(err);
+    show('gh-error-template', { message: err });
+  }
 })();
+
+//need to have one central parsing function that listens for the input
+//and calls other functions based on the listener @gh, @weather etc.
