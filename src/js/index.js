866f9c1e615c580a448abb72869c15f9fe28adbc
Andy Baird <Andy.Baird@icloud.com>
2015-06-04 21:41:27 -0400

    added function for unique li generation with user input

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,21 @@
 (function() {
 
-  $('.user-form').on('submit', function(e){
+//  $('.user-form').on('submit', function(e){
+//    e.preventDefault();
+//    var input = $('.user-input').val();
+//    $('.list-user').text(input); 
+//    $('.user-input').val(''); 
+//  });
+
+  $('.user-form').on('submit', function newLi(e){
     e.preventDefault();
-    var input = $('.user-input').val();
-    $('.list-user').text(input); 
-    $('.user-input').val(''); 
+        var ul = document.querySelector('.output-list');
+        var li = document.createElement("li");
+        var input = $('.user-input').val();
+          li.setAttribute('class','list-user');
+          ul.appendChild(li);
+          $('.list-user').last().text(input);
+          $('.user-input').val(''); 
   });
 
 
